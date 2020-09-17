bf5a5d9e9138071f7e7be46260c824b4188378ea
Sascha Engmann <engmann@w11k.de>
2020-09-17 18:05:24 +0200

    Library hinzugefügt und Header Komponente geschrieben

--- rail/apps/prototype/src/app/app.module.ts
+++ rail/apps/prototype/src/app/app.module.ts
@@ -2,10 +2,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CorporateModule } from "@rail/corporate";
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, CorporateModule],
   providers: [],
   bootstrap: [AppComponent],
 })
